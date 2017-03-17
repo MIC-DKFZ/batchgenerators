438c6eee516c2ad6ccd88ceae2671f962a6daf95
wasserth <j.wasserthal@dkfz.de>
2017-03-17 17:02:14 +0100

    Add unit test for testing reproducibility by seeding multithreaded generators

--- DeepLearningBatchGeneratorUtils/tests/MockBatchGenerator.py
+++ DeepLearningBatchGeneratorUtils/tests/MockBatchGenerator.py
@@ -5,8 +5,11 @@ class MockBatchGenerator(BatchGeneratorBase):
 
     def generate_train_batch(self):
 
-        x = np.array(self._data[0][0:self.BATCH_SIZE])
-        y = np.array(self._data[1][0:self.BATCH_SIZE])
+        #Sample randomly from data
+        idx = np.random.choice(self._data[0].shape[0], self.BATCH_SIZE, False, None)
+        # copy data to ensure that we are not modifying the original dataset with subsequeng augmentation techniques!
+        x = np.array(self._data[0][idx])
+        y = np.array(self._data[1][idx])
 
         data_dict = {"data": x,
                      "seg": y}
--- DeepLearningBatchGeneratorUtils/tests/test_multithreading.py
+++ DeepLearningBatchGeneratorUtils/tests/test_multithreading.py
@@ -6,22 +6,48 @@ from DeepLearningBatchGeneratorUtils.MultiThreadedGenerator import MultiThreaded
 class TestMultiThreading(unittest.TestCase):
 
     def setUp(self):
-        self.x = np.random.rand(100, 5)
+        self.seed = 1234
+        np.random.seed(self.seed)
+        self.x = np.random.rand(100, 2)
         self.y = np.random.rand(100, 1)
 
-    def test_test1(self):
-        pass
-
     def test_nr_of_batches(self):
-
         NR_THREADS = 5
         NR_BATCHES_PER_THREAD = 10
 
-        batch_gen = MockBatchGenerator((self.x, self.y), BATCH_SIZE=8, num_batches=NR_BATCHES_PER_THREAD, seed=None)
-        batch_gen = MultiThreadedGenerator(batch_gen, NR_THREADS, NR_THREADS, seeds=[1234] * NR_THREADS)
+        batch_gen = MockBatchGenerator((self.x, self.y), BATCH_SIZE=8, num_batches=NR_BATCHES_PER_THREAD, seed=self.seed)
+        batch_gen = MultiThreadedGenerator(batch_gen, NR_THREADS, NR_THREADS, seeds=[self.seed] * NR_THREADS)
         ctr = 0
         for batch in batch_gen:
             ctr += 1
         print("generated " + str(ctr) + " batches")
 
         self.assertEqual(ctr, NR_THREADS*NR_BATCHES_PER_THREAD, "number of batches do not match")
+
+
+    def test_seeding_of_multi_threaded(self):
+        '''
+        Test if seeding works.
+
+        Test if
+        1. multiple threads always return in same order
+        2. random sampling of testing data always returns same data
+        '''
+        NR_THREADS = 3
+        NR_BATCHES_PER_THREAD = 1
+
+        batch_gen = MockBatchGenerator((self.x, self.y), BATCH_SIZE=1, num_batches=NR_BATCHES_PER_THREAD, seed=False) #seed has to be False here!
+        # generate list with increasing seed e.g. [1234, 1235, 1236], so we have different seed for every thread (but reproducible)
+        seeds = [seed+idx for idx, seed in enumerate([self.seed]*NR_THREADS)]
+        batch_gen = MultiThreadedGenerator(batch_gen, NR_THREADS, NR_THREADS, seeds=seeds)
+        results = []
+        for batch in batch_gen:
+            results.append(batch["data"])
+        results = np.array(results)
+
+        true_results = [[[0.43689317, 0.612149]],
+                        [[0.39720258, 0.78873014]],
+                        [[0.90179605, 0.70652816]]]
+
+        elementwise_equality = np.isclose(results, true_results)   #isclose comparison needed for float
+        self.assertTrue(np.all(elementwise_equality), "Order and/or content of MultiThreadedGenerator not reproducible")
