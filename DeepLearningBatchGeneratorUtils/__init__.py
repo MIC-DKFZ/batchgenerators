c1bd60b472a1752d94814f35843fdf46f9e2d9f3
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2017-05-12 15:22:49 +0200

    fixed imports

--- DeepLearningBatchGeneratorUtils/__init__.py
+++ DeepLearningBatchGeneratorUtils/__init__.py
@@ -2,11 +2,9 @@ __author__ = 'Fabian Isensee'
 
 from DataGeneratorBase import BatchGeneratorBase
 from MultiThreadedGenerator import MultiThreadedGenerator
-from SpatialTransformGenerators import (seg_channel_selection_generator, center_crop_generator,
-                                        elastric_transform_generator,
-                                        center_crop_seg_generator, random_crop_generator,
-                                        rotation_generator, rotation_and_elastic_transform_generator,
-                                        data_channel_selection_generator,
-                                        pad_generator, mirror_axis_generator, rescale_and_crop_generator)
+import SpatialTransformGenerators
+import ColorAugmentationGenerators
+import CropAndPadGenerators
+import ChannelSelectionGenerators
 from utils import *
 from util_generators import *
