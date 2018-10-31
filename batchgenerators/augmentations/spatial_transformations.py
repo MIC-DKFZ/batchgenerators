c16f7f63ca0810381111ba9f7e01b75b7260b3e4
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2018-10-31 11:50:14 +0100

    rendom crop revamp. Now supports negative margins (data and seg will be zero padded), the rondom crop location is chosen for each sample instead of once per batch (data=array only). We now do not have to distinguish data/seg=list and data/seg=array anymore in the code -> much cleaner. If crop_size > data_size then we also pad with 0

--- batchgenerators/augmentations/spatial_transformations.py
+++ batchgenerators/augmentations/spatial_transformations.py
@@ -367,7 +367,8 @@ def augment_spatial(data, seg, patch_size, patch_center_dist_from_border=30,
             else:
                 s = seg[sample_id:sample_id + 1]
             if random_crop:
-                d, s = random_crop_aug(data[sample_id:sample_id + 1], s, patch_size, patch_center_dist_from_border)
+                margin = [patch_center_dist_from_border[d] - patch_size[d] // 2 for d in range(dim)]
+                d, s = random_crop_aug(data[sample_id:sample_id + 1], s, patch_size, margin)
             else:
                 d, s = center_crop_aug(data[sample_id:sample_id + 1], patch_size, s)
             data_result[sample_id] = d[0]
