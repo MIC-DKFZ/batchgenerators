11fe8894595b7b6cf885d65b45142deb61cbebfa
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2018-11-09 15:40:39 +0100

    noise transforms

--- batchgenerators/augmentations/utils.py
+++ batchgenerators/augmentations/utils.py
@@ -644,3 +644,49 @@ def pad_nd_image(image, new_shape=None, mode="edge", kwargs=None, return_slicer=
         slicer = list(slice(*i) for i in pad_list)
         return res, slicer
 
+
+def mask_random_square(img, square_size, n_val, channel_wise_n_val=False, square_pos=None):
+    """Masks (sets = 0) a random square in an image"""
+
+    img_h = img.shape[-2]
+    img_w = img.shape[-1]
+
+    img = img.copy()
+
+    if square_pos is None:
+        w_start = np.random.randint(0, img_w - square_size)
+        h_start = np.random.randint(0, img_h - square_size)
+    else:
+        pos_wh = square_pos[np.random.randint(0, len(square_pos))]
+        w_start = pos_wh[0]
+        h_start = pos_wh[1]
+
+    if img.ndim == 2:
+        rnd_n_val = get_range_val(n_val)
+        img[h_start:(h_start + square_size), w_start:(w_start + square_size)] = rnd_n_val
+    elif img.ndim == 3:
+        if channel_wise_n_val:
+            for i in range(img.shape[0]):
+                rnd_n_val = get_range_val(n_val)
+                img[i, h_start:(h_start + square_size), w_start:(w_start + square_size)] = rnd_n_val
+        else:
+            rnd_n_val = get_range_val(n_val)
+            img[:, h_start:(h_start + square_size), w_start:(w_start + square_size)] = rnd_n_val
+    elif img.ndim == 4:
+        if channel_wise_n_val:
+            for i in range(img.shape[0]):
+                rnd_n_val = get_range_val(n_val)
+                img[:, i, h_start:(h_start + square_size), w_start:(w_start + square_size)] = rnd_n_val
+        else:
+            rnd_n_val = get_range_val(n_val)
+            img[:, :, h_start:(h_start + square_size), w_start:(w_start + square_size)] = rnd_n_val
+
+    return img
+
+
+def mask_random_squares(img, square_size, n_squares, n_val, channel_wise_n_val=False, square_pos=None):
+    """Masks a given number of squares in an image"""
+    for i in range(n_squares):
+        img = mask_random_square(img, square_size, n_val, channel_wise_n_val=channel_wise_n_val,
+                                 square_pos=square_pos)
+    return img
