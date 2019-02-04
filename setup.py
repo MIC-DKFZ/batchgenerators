ad13f1c80b481e0a3768e5bb31c1cf642b9ebc1e
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2019-02-04 12:56:43 +0100

    modified setuip.py and setup.cfg for release

--- setup.py
+++ setup.py
@@ -7,8 +7,7 @@ setup(name='batchgenerators',
       author='Division of Medical Image Computing, German Cancer Research Center',
       author_email='f.isensee@dkfz-heidelberg.de',
       license='Apache License Version 2.0, January 2004',
-      packages=['batchgenerators', 'batchgenerators.augmentations',
-      'batchgenerators.examples', 'batchgenerators.transforms', 'batchgenerators.dataloading'],
+      packages=['batchgenerators'],
       install_requires=[
             "numpy>=1.10.2",
             "scipy",
@@ -16,4 +15,25 @@ setup(name='batchgenerators',
             "future",
             "unittest2"
       ],
-      zip_safe=False)
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+          'Intended Audience :: Developers',  # Define that your audience are developers
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: MIT License',  # Again, pick a license
+          'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 2.7',
+      ],
+      keywords = ['deep learning', 'image segmentation', 'image classification', 'medical image analysis',
+                  'medical image segmentation', 'data augmentation'],
+      )
+
+setup(
+
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+
+
+)
