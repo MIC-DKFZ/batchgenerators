a7b4f78c2a527f8633827675f07399a5966044c5
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2019-04-24 13:40:40 +0200

    0.19.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='batchgenerators',
-      version='0.19.2',
+      version='0.19.3',
       description='Data augmentation toolkit',
       url='https://github.com/MIC-DKFZ/batchgenerators',
       author='Division of Medical Image Computing, German Cancer Research Center',
