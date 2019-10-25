2d16fbfb6e2a1d84b41c4ef6b554f09af9c1bac5
Fabian Isensee <f.isensee@dkfz-heidelberg.de>
2019-10-25 10:54:22 +0200

    force forkserver

--- batchgenerators/__init__.py
+++ batchgenerators/__init__.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
+import multiprocessing
+multiprocessing.set_start_method('forkserver', force=True)
 
 import batchgenerators.augmentations
 from batchgenerators.augmentations import utils
 import batchgenerators.transforms
 import batchgenerators.dataloading
 import batchgenerators.utilities
+
