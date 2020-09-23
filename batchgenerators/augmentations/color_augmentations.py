c73f07d5f01b5982a130d5e68cfb71e425edb0de
Fabian Isensee <isenseef@gmail.com>
2020-09-23 07:06:26 +0200

    fixed a bug, tank you @LucasFidon!

--- batchgenerators/augmentations/color_augmentations.py
+++ batchgenerators/augmentations/color_augmentations.py
@@ -98,8 +98,9 @@ def augment_gamma(data_sample, gamma_range=(0.5, 2), invert_image=False, epsilon
         rnge = data_sample.max() - minm
         data_sample = np.power(((data_sample - minm) / float(rnge + epsilon)), gamma) * rnge + minm
         if retain_stats:
-            data_sample = data_sample - data_sample.mean() + mn
+            data_sample = data_sample - data_sample.mean()
             data_sample = data_sample / (data_sample.std() + 1e-8) * sd
+            data_sample = data_sample + mn
     else:
         for c in range(data_sample.shape[0]):
             if retain_stats:
@@ -113,8 +114,9 @@ def augment_gamma(data_sample, gamma_range=(0.5, 2), invert_image=False, epsilon
             rnge = data_sample[c].max() - minm
             data_sample[c] = np.power(((data_sample[c] - minm) / float(rnge + epsilon)), gamma) * float(rnge + epsilon) + minm
             if retain_stats:
-                data_sample[c] = data_sample[c] - data_sample[c].mean() + mn
+                data_sample[c] = data_sample[c] - data_sample[c].mean()
                 data_sample[c] = data_sample[c] / (data_sample[c].std() + 1e-8) * sd
+                data_sample[c] = data_sample[c] + mn
     if invert_image:
         data_sample = - data_sample
     return data_sample
